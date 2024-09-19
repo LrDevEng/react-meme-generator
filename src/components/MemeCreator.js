8b00286e746e117cfc43feae56530ce9e2176fc6
LrDevEng <lr.deveng@gmail.com>
2024-09-19 15:16:59 +0200

    Update to pass drone test

--- src/components/MemeCreator.js
+++ src/components/MemeCreator.js
@@ -70,23 +70,23 @@ function MemeCreator() {
   }
 
   // Parsers
+  function parseApiRequest(preview) {
+    if (preview) {
+      const previewURL = 'https://api.memegen.link/images/preview.jpg';
+      const requestUrl = `${previewURL}?template=${currentMemeId}&lines[]=${encodeToUrl(topText)}&lines[]=${encodeToUrl(bottomText)}`;
+      return requestUrl;
+    } else {
+      return `https://api.memegen.link/images/${currentMemeId}/${encodeToUrl(topText)}/${encodeToUrl(bottomText)}.png`;
+    }
+  }
+
   // function parseApiRequest(preview) {
   //   if (preview) {
-  //     const previewURL = 'https://api.memegen.link/images/preview.jpg';
-  //     const requestUrl = `${previewURL}?template=${currentMemeId}&lines[]=${topText}&lines[]=${bottomText}`;
-  //     return requestUrl;
+  //     return `https://api.memegen.link/images/${currentMemeId}/${encodeToUrl(topText)}/${encodeToUrl(bottomText)}.png`;
   //   } else {
   //     return `https://api.memegen.link/images/${currentMemeId}/${topText}/${bottomText}.png`;
   //   }
   // }
-
-  function parseApiRequest(preview) {
-    if (preview) {
-      return `https://api.memegen.link/images/${currentMemeId}/${encodeToUrl(topText)}/${encodeToUrl(bottomText)}.png`;
-    } else {
-      return `https://api.memegen.link/images/${currentMemeId}/${topText}/${bottomText}.png`;
-    }
-  }
 
   function encodeToUrl(text) {
     if (text.length === 0) return '_';
